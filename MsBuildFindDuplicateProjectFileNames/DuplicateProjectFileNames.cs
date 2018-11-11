85ffc32cbf140a9e8269aeb6a79504747b4538cf
Ace Olszowka <aolszowka@gmail.com>
2018-11-11 11:43:06 -0700

    Initial commit of the utility

---
+++ MsBuildFindDuplicateProjectFileNames/DuplicateProjectFileNames.cs
@@ -0,0 +1,74 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DuplicateProjectFileNames.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildFindDuplicateProjectFileNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    static class DuplicateProjectFileNames
+    {
+        internal static IEnumerable<KeyValuePair<string, IDictionary<string, string>>> Execute(string targetDirectory)
+        {
+            IDictionary<string, IDictionary<string, string>> pfnDictionary = LoadProjectFileNameDictionary(targetDirectory);
+
+            // Now filter the above dictionary just to places where we have duplicates
+            IEnumerable<KeyValuePair<string, IDictionary<string, string>>> duplicatedProjectNamesEntries = pfnDictionary.Where(kvp => kvp.Value.Count > 1);
+
+            return duplicatedProjectNamesEntries;
+        }
+
+        private static IDictionary<string, IDictionary<string, string>> LoadProjectFileNameDictionary(string targetDirectory)
+        {
+            IEnumerable<string> projectsInDirectory = GetProjectsInDirectory(targetDirectory);
+
+            IDictionary<string, IDictionary<string, string>> distinctProjects = new Dictionary<string, IDictionary<string, string>>(StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (string projectPath in projectsInDirectory)
+            {
+                // Get the GUID of this project
+                string projectGuid = MSBuildUtilities.GetMSBuildProjectGuid(projectPath);
+
+                // Now get the name of the project
+                string projectName = Path.GetFileNameWithoutExtension(projectPath);
+
+                IDictionary<string, string> duplicatedProjects;
+
+                if (distinctProjects.ContainsKey(projectName))
+                {
+                    duplicatedProjects = distinctProjects[projectName];
+                }
+                else
+                {
+                    duplicatedProjects = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+                    distinctProjects.Add(projectName, duplicatedProjects);
+                }
+
+                duplicatedProjects.Add(projectPath, projectGuid);
+            }
+
+            return distinctProjects;
+        }
+
+        /// <summary>
+        /// Gets all Project Files that are understood by this
+        /// tool from the given directory and all subdirectories.
+        /// </summary>
+        /// <param name="targetDirectory">The directory to scan for projects.</param>
+        /// <returns>All projects that this tool supports.</returns>
+        static IEnumerable<string> GetProjectsInDirectory(string targetDirectory)
+        {
+            HashSet<string> supportedFileExtensions = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase) { ".csproj", ".vbproj", ".synproj" };
+
+            return
+                Directory
+                .EnumerateFiles(targetDirectory, "*proj", SearchOption.AllDirectories)
+                .Where(currentFile => supportedFileExtensions.Contains(Path.GetExtension(currentFile)));
+        }
+    }
+}
---
+++ MsBuildFindDuplicateProjectFileNames/Program.cs
@@ -0,0 +1,87 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildFindDuplicateProjectFileNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int errorCode = 0;
+
+            if (args.Any())
+            {
+                string command = args.First().ToLowerInvariant();
+
+                if (command.Equals("-?") || command.Equals("/?") || command.Equals("-help") || command.Equals("/help"))
+                {
+                    errorCode = ShowUsage();
+                }
+                else
+                {
+                    if (Directory.Exists(command))
+                    {
+                        string targetPath = command;
+                        errorCode = PrintToConsole(command);
+                    }
+                    else
+                    {
+                        string error = string.Format("The specified path `{0}` is not valid.", command);
+                        Console.WriteLine(error);
+                        errorCode = 1;
+                    }
+                }
+            }
+            else
+            {
+                // This was a bad command
+                errorCode = ShowUsage();
+            }
+
+            Environment.Exit(errorCode);
+        }
+
+        private static int ShowUsage()
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendLine("Scans given directory for MsBuild Projects; reporting any duplicates it finds.");
+            message.AppendLine("Invalid Command/Arguments. Valid commands are:");
+            message.AppendLine();
+            message.AppendLine("[directory]    - [READS] Spins through the specified directory and all\n" +
+                               "                 subdirectories for Project files; prints any duplicate\n" +
+                               "                 names it finds. Returns the number of duplicates.");
+            Console.WriteLine(message);
+            return 21;
+        }
+
+        static int PrintToConsole(string targetDirectory)
+        {
+            int duplicatedProjectCount = 0;
+            IEnumerable<KeyValuePair<string, IDictionary<string, string>>> duplicateProjectFileNames = DuplicateProjectFileNames.Execute(targetDirectory);
+
+            foreach (KeyValuePair<string, IDictionary<string, string>> duplicateProjectFileNameEntry in duplicateProjectFileNames)
+            {
+                duplicatedProjectCount++;
+                Console.WriteLine($"{duplicateProjectFileNameEntry.Key}:");
+
+                foreach (KeyValuePair<string, string> duplicatedProjectEntry in duplicateProjectFileNameEntry.Value)
+                {
+                    Console.WriteLine($"{duplicatedProjectEntry.Key}\t{duplicatedProjectEntry.Value}");
+                }
+
+                Console.WriteLine();
+            }
+
+            return duplicatedProjectCount;
+        }
+    }
+}
